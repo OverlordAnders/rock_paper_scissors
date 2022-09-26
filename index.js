fcc9ca3a801531010f1f586ceb7e566761965293
Brina <landrybrina@gmail.com>
2022-09-26 19:51:15 -0400

    got my first function working in the index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function getComputerChoice (){
+ const choice = ["Rock", "Paper", "Scissors"]
+ return choice[Math.floor(Math.random() * choice.length)];
+}
+function playRound (playerSelection, computerSelection) {
+
+}
--- javascript/index.js
+++
@@ -1,6 +0,0 @@
-const arr =["Rock","Paper","Scissors"]
-function getComputerChoice (){
- return arr[Math.floor(Math.Random() * arr.length)]
-}
-
-console.log(getComputerChoice);
